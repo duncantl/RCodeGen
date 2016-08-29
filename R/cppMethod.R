d94afab833d1e57bf8c8046454ccd9ff09dbee13
Duncan Temple Lang <duncan@r-project.org>
2016-08-29 10:55:04 -0700

    more support for constructor, destructor and C++ methods generally

--- R/cppMethod.R
+++ R/cppMethod.R
@@ -1,25 +1,35 @@
 createCppMethod =
     #
-    # This is for RCIndex.
+    # This is for objects created by RCIndex (not RGCCTranslationUnit)
     # 
 function(mdef, className = getName(getCursorLexicalParent(mdef@def)), classPrefix = "",
-          typeMap = NULL)
+          typeMap = NULL, allClassMethods = NULL)
 {
 
   isStatic = getCursorTokens(mdef@def)[1] == "static"
-
+  isConstructor = is(mdef, "C++ClassConstructor")
+  
     # The name of the R proxy routine.
-  Rname = sprintf("R_%s_%s", className, mdef@name)
+  Rname = getNativeProxyName(mdef, classMethods = allClassMethods)  # sprintf("R_%s_%s", className, mdef@name)
 
   params = mdef@params
+
+    # Fix up missing names.
+  if(length(params)) {
+      pnames = names(params)
+      if(is.null(pnames))
+         names(params) = LETTERS[seq(along = params)]
+      else if(any( i <- (pnames == "")))
+         names(params)[i] = sprintf("param_%d", seq(1, sum(i)))
+  }
 
     # The signature for the routine, adding r_tthis if this is a member method.
   decl = sprintf("SEXP %s(%s%s%s)" ,
                    Rname,
-                   if(isStatic) "" else "SEXP r_tthis",
-                   if(length(params) && !isStatic) ", " else "",
+                   if(isStatic || isConstructor) "" else "SEXP r_tthis",
+                   if(length(params) && !(isStatic || isConstructor)) ", " else "",
                    if(length(params))
-                     paste("SEXP", paste0("r_", names(params)), sep = " ", collapse = ", ")
+                      paste("SEXP", paste0("r_", names(params)), sep = " ", collapse = ", ")
                    else "")
   
   if(length(mdef@params))
@@ -31,12 +41,23 @@ function(mdef, className = getName(getCursorLexicalParent(mdef@def)), classPrefi
     # Add code for constructor and destructor methods
   call = if(isStatic)
             sprintf("%s::%s(%s);", className, mdef@name, cargs)
+         else if(isConstructor)
+            sprintf("new %s(%s);", className, cargs)            
+         else if(is(mdef, "C++ClassDestructor"))
+            "delete tthis;"
          else
             sprintf("tthis->%s(%s);", mdef@name, cargs)
   
   retType = getResultType(mdef@def$type)
-  ansDecl = getNativeDeclaration("ans", retType, typeMap = typeMap)
+  ansDecl = if(isConstructor)
+               sprintf("%s *ans;", className)
+            else
+               getNativeDeclaration("ans", retType, typeMap = typeMap)
 
+  rresult =  if(isConstructor)
+                 sprintf('createRef(ans, "%s", NULL)', className) #XXX add the Ptr or not?   Add the finalizer.
+             else
+                convertValueToR(retType, "ans", typeMap = typeMap, rvar = "r_ans")
 
   if(length(params)) {
     args = mapply(createInitLocalVar, names(params),
@@ -46,16 +67,15 @@ function(mdef, className = getName(getCursorLexicalParent(mdef@def)), classPrefi
   } else
     args = character()
   
-  # static method is different
-  code = c(if(!isStatic)
+  # static and constructors and destructor methods are different
+  code = c(if(!(isStatic || isConstructor))
               sprintf("%s%s *tthis = GET_REF(r_tthis, %s);", classPrefix, className, className),
-           if(isVoidType(retType))
+           if(!isConstructor && isVoidType(retType))
              c(args, call, "return(R_NilValue);")
            else 
              c(args, ansDecl,
                sprintf("ans = %s", call),
-               sprintf("SEXP r_ans = %s;",
-                          convertValueToR(retType, "ans", typeMap = typeMap, rvar = "r_ans")),
+               sprintf("SEXP r_ans = %s;", rresult),
                "return(r_ans);")
               )
 
