5be144ed0ecfbb8b234c0ee88e2f4e3dc5f52354
Duncan Temple Lang <duncan@r-project.org>
2016-08-29 10:52:40 -0700

    format the result for convertValue if a character in the typemap

--- R/typeMap.R
+++ R/typeMap.R
@@ -2,6 +2,7 @@ lookupTypeMap =
   # rvar is the name of the SEXP target variable
 function(map, typeName, what, type, ..., cast = "")
 {
+   
    typeName = gsub("^const ", "", typeName)
    typeName = gsub("[[:space:]]*&$", "", typeName)   
 
@@ -22,9 +23,11 @@ function(map, typeName, what, type, ..., cast = "")
    if(is.function(val))
       val(..., type = type, typeMap = map, cast = cast)
    else if(is.character(val)) {
+#      if(val == "GooStringFromR") browser()       
       args = list(...)
       switch(what,
              convertValueToR = sprintf("%s%s(%s)", cast, val, args[[1]]),
+             convertRValue = sprintf("%s = %s%s(%s);", args[[1]], cast, val, args[[2]]),
              character())
    }
 }
